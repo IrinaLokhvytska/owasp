34a8d434c1aa585f38ff1ae19b8a4bdd16ae102b
ilokhvytska-spwr <iryna.lokhvytska@sunpowercorp.com>
2021-09-12 20:28:37 +0300

    add user model

---
+++ backend/web/models/user.py
@@ -0,0 +1,22 @@
+""" User db model """
+import datetime
+
+from web import app, db
+
+
+class User(db.Model):
+    """ User Model for storing user related details """
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, email, password, admin=False):
+        """ Init User db model """
+        self.email = email
+        self.password = self.password
+        self.registered_on = datetime.datetime.now()
+        self.admin = admin
---
+++ backend_secure/web/models/user.py
@@ -0,0 +1,51 @@
+""" User db model """
+import datetime
+import secrets
+
+from argon2 import PasswordHasher
+
+from web import app, db
+
+
+class User(db.Model):
+    """ User Model for storing user related details """
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    salt = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, email, password, admin=False):
+        """ Init User db model """
+        self.email = email
+        self.salt = self.get_salt_for_new_password()
+        self.password = self.hash_new_password(password, self.salt)
+        self.registered_on = datetime.datetime.now()
+        self.admin = admin
+    
+    def get_salt_for_new_password():
+        """ Get salt for new password """
+        alphabet = string.ascii_letters + string.digits
+        salt = ''.join(secrets.choice(alphabet) for i in range(8))
+        return salt
+    
+    def hash_new_password(password, salt):
+        """
+        Hash the provided password with a randomly-generated salt and return the
+        salt and hash to store in the database.
+        """
+        pepper = app.config["SECRET_KEY"]
+        ph = PasswordHasher()
+        pw_hash = ph.hash(password + salt + pepper)
+        return salt, pw_hash
+
+    def is_correct_password(salt, pw_hash, password):
+        """
+        Given a previously-stored salt and hash, and a password provided by a user
+        trying to log in, check whether the password is correct.
+        """
+        ph = PasswordHasher()
+        return ph.verify(pw_hash, password)
